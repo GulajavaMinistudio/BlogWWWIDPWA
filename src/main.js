f50e216c196da503a91dba9561908310ff678c7b
GulajavaMinistudio <gulajava.mini@gmail.com>
2018-04-23 14:20:48 +0700

    Penambahan animasi untuk CSS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import 'jquery';
 import Vue from 'vue';
 import App from './App';
-import router from './router';// import '../semantic/dist/semantic.min.css';
+import router from './router';
+// import '../semantic/dist/semantic.min.css';
 // import '../semantic/dist/semantic.min';
 import '../src/librarys/progressive-image.min.css';
 import '../src/librarys/progressive-image.min';
